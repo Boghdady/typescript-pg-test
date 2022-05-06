925549b9e6bf8fb49c70d6db63d10657eb8a5619
boghdady <boghdady107@gmail.com>
2022-05-06 07:25:54 +0200

    Use case 4- Modify the code or database to not allow duplicate users

--- server.ts
+++ server.ts
@@ -55,7 +55,7 @@ const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
 
 db.none(
-  'DROP TABLE IF EXISTS github_users; CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, type TEXT, site_admin Boolean)'
+  'DROP TABLE IF EXISTS github_users; CREATE TABLE github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, type TEXT, site_admin Boolean); CREATE UNIQUE INDEX users_indexes ON github_users(login, name, company, type, site_admin)'
 )
   .then(() =>
     request({
