b55c59bfa6157f185b71cb00bcfebdbee97603b2
boghdady <boghdady107@gmail.com>
2022-05-13 05:22:43 +0200

    Save config values in ENV Or JSON file

--- config/pg-options.ts
+++ config/pg-options.ts
@@ -1,12 +1,19 @@
 import { DBOptions } from '../dtos/db-options.dto';
+const {
+  postgres_user,
+  postgres_password,
+  postgres_port,
+  postgres_host,
+  postgres_database,
+} = require('./config.json');
 
 // Actual database options
 const options: DBOptions = {
-  user: 'postgres',
-  password: 'postgres',
-  port: 5432,
-  host: 'postgres',
-  database: 'lovelystay_test',
+  user: postgres_user,
+  password: postgres_password,
+  port: postgres_port,
+  host: postgres_host,
+  database: postgres_database,
 };
 
 console.info(
